5a1d4ed63c158a92175db8e2b2bb7b6e78189f79
amasin76 <amasin76@gmail.com>
2023-08-07 21:47:00 +0000

    Implement: BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Initialize a new BaseModel instance"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance"""
+        return "[{}] ({}) {}".\
+            format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance"""
+        d = self.__dict__.copy()
+        d["__class__"] = self.__class__.__name__
+        d["created_at"] = self.created_at
+        d["updated_at"] = self.updated_at
+        return d
