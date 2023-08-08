bc18eb8cbcc42baceae3fbb99f42e3a4793c2fe8
Amilemia <aminemadah2@gmail.com>
2023-08-08 23:43:05 +0100

    Edit: Deserialisation, ISO strings support

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,23 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
-        return "[{}] ({}) {}".\
-            format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
         """Update the updated_at attribute with the current datetime"""
@@ -24,8 +31,8 @@ class BaseModel:
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance"""
-        d = self.__dict__.copy()
-        d["__class__"] = self.__class__.__name__
-        d["created_at"] = self.created_at
-        d["updated_at"] = self.updated_at
-        return d
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
