d6d7e3bfeb13a5a633fdb67db01ec6be94a0a3a8
amasin76 <amasin76@gmail.com>
2023-08-12 16:45:58 +0000

    Add: support for dot notation commands

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """This module contains the entry point of the command interpreter"""
 
 import cmd
+import re
+from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
@@ -39,6 +41,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """An empty line + ENTER shouldn’t execute anything"""
         pass
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_create(self, arg):
         """Create a new instance of BaseModel, save it, and print the id"""
@@ -135,6 +158,51 @@ class HBNBCommand(cmd.Cmd):
                 attr_value = attr_type(args[3].strip('"'))
                 setattr(obj, attr_name, attr_value)
                 obj.save()
+
+    def do_all(self, arg):
+        """
+        Display string representations of all instances of a given class
+        If no class is specified, displays all instantiated objects
+        """
+        argl = parse(arg)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in storage.all().values():
+                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(argl) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
+
+    def do_count(self, arg):
+        """Retrieve the number of instances of a given class"""
+
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
+
+
+def parse(arg):
+    """
+    Parse a string of arguments and return a list of tokens
+    """
+    tokens = []
+    while arg:
+        match = re.search(r'(\{.*?\}|\[.*?\])', arg)
+        if match:
+            start, end = match.span()
+            tokens.extend(arg[:start].split())
+            tokens.append(arg[start:end])
+            arg = arg[end:]
+        else:
+            tokens.extend(arg.split())
+            break
+    return tokens
 
 
 if __name__ == '__main__':
