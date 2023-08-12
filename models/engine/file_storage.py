c2654b20757715aa7c790face43d920d64e217a9
amasin76 <amasin76@gmail.com>
2023-08-12 14:07:37 +0000

    Update: `FileStorage` to handle new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,9 +34,24 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 obj_dict = json.load(f)
                 from models.base_model import BaseModel
+                from models.user import User
+                from models.state import State
+                from models.city import City
+                from models.amenity import Amenity
+                from models.place import Place
+                from models.review import Review
+                classes = {
+                    'BaseModel': BaseModel,
+                    'User': User,
+                    'State': State,
+                    'City': City,
+                    'Amenity': Amenity,
+                    'Place': Place,
+                    'Review': Review
+                }
                 for key, value in obj_dict.items():
                     class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
+                    cls = classes[class_name]
                     new_obj = cls(**value)
                     self.new(new_obj)
         except FileNotFoundError:
